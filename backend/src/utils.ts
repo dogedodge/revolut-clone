47aeb36049df10491b3180e6d3b9256b79a84ff0
james.ye <binbinye2@gmail.com>
2024-07-28 09:37:02 +0100

    add cach mechanism

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,7 +1,13 @@
 import fs from 'fs/promises';
 import path from 'path';
 
+const queryCache: { [key: string]: Promise<string> } = {};
 export async function loadSqlQuery(filePath: string) {
+  const cache = queryCache[filePath];
+  if (cache) return cache;
+
   const fullPath = path.join(__dirname, '..', 'mysql', `${filePath}.sql`);
-  return fs.readFile(fullPath, 'utf-8');
+  const result = fs.readFile(fullPath, 'utf-8');
+  queryCache[filePath] = result;
+  return result;
 }
