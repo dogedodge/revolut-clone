5104ff85dc36ab5d6a76a82f2ec419fc3c913ce3
james.ye <binbinye2@gmail.com>
2024-08-14 22:07:13 +0800

    /api/login

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,28 @@
 import express from 'express';
+import { createConnectionPool } from './db/createConnectionPool';
+import { DBContext } from './db/DBContext';
+import { userLogin } from './db/userLogin';
 
 const app = express();
 
+app.use(express.json());
+
 app.get(`/`, (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
+});
+
+const pool = createConnectionPool();
+
+app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
+  const dbCtx: DBContext = { pool };
+  try {
+    const user = await userLogin(dbCtx, email, password);
+    res.json(user);
+  } catch (err) {
+    const { message } = err as Error;
+    res.status(401).json({ message });
+  }
 });
 
 export default app;
