ce1d80ae080f2e704ecf6127b457425b85fb219f
james.ye <binbinye2@gmail.com>
2024-08-16 21:33:16 +0800

    bug fix

--- backend/src/tools/runSqlFile.ts
+++ backend/src/tools/runSqlFile.ts
@@ -7,7 +7,7 @@ async function loadSqlQuery(filePath: string) {
   const cache = queryCache[filePath];
   if (cache) return cache;
 
-  const fullPath = path.join(__dirname, '..', 'mysql', `${filePath}.sql`);
+  const fullPath = path.join(__dirname, '../..', 'mysql', `${filePath}.sql`);
   const result = fs.readFile(fullPath, 'utf-8');
   queryCache[filePath] = result;
   return result;
