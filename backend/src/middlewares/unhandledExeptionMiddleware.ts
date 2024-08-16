48d8d1714293ba96656e19a97b19a7ae5175240d
james.ye <binbinye2@gmail.com>
2024-08-16 17:00:23 +0800

    correct file name

---
+++ backend/src/middlewares/unhandledExeptionMiddleware.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+
+export const unhandledExeptionMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.log('unhandledExeptionMiddleware');
+  const { message } = err as Error;
+  console.error(err);
+  res.status(500).json({ code: 500, message });
+};
