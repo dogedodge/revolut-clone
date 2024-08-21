f44e9694a9ebcb16f18d6384cffca0a15889f9ce
james.ye <binbinye2@gmail.com>
2024-08-21 16:52:10 +0800

    smaller fractional part for decimal number

--- frontend/src/component/AccountBalanceDisplay.stories.ts
+++ frontend/src/component/AccountBalanceDisplay.stories.ts
@@ -15,5 +15,7 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     // variant: 'add-money',
+    currency: 'GBP',
+    balance: 105.18,
   },
 };
--- frontend/src/component/AccountBalanceDisplay.tsx
+++ frontend/src/component/AccountBalanceDisplay.tsx
@@ -1,8 +1,25 @@
-const AccountBalanceDisplay = () => {
+interface AccountBalanceDisplayProps {
+  currency: string;
+  balance: string | number;
+  onClick?: () => void;
+  className?: string;
+}
+
+const AccountBalanceDisplay: React.FC<AccountBalanceDisplayProps> = ({
+  currency,
+  balance,
+}) => {
+  const { integer, fractional } = decomponseDecimal(balance);
+  const currencySymbol = '£'; // todo
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="text-white font-extralight text-lg mb-1">Main · GBP</div>
-      <div className="text-white text-5xl font-semibold mb-4">£105.18</div>
+      <div className="text-white font-extralight text-lg mb-1">
+        Main · {currency}
+      </div>
+      <div className="text-white font-semibold mb-4">
+        <span className="text-5xl">{`${currencySymbol}${integer}`}</span>
+        <span className="text-lg">{fractional}</span>
+      </div>
       <div className="bg-indigo-400 text-white text-base px-4 py-2 rounded-full">
         Accounts
       </div>
@@ -11,3 +28,12 @@ const AccountBalanceDisplay = () => {
 };
 
 export default AccountBalanceDisplay;
+
+function decomponseDecimal(decimal: string | number) {
+  const nString = typeof decimal === 'number' ? `${decimal}` : decimal;
+  const parts = nString.split('.');
+  return {
+    integer: parts[0],
+    fractional: parts.length === 2 ? `.${parts[1]}` : '',
+  };
+}
