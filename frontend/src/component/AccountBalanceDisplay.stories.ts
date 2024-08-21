111655c4d63c99397f7635c9ff26cb08a3667707
james.ye <binbinye2@gmail.com>
2024-08-21 20:20:24 +0800

    fix onclick spy

--- frontend/src/component/AccountBalanceDisplay.stories.ts
+++ frontend/src/component/AccountBalanceDisplay.stories.ts
@@ -1,12 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
-// import { fn } from '@storybook/test';
+import { fn } from '@storybook/test';
 import AccountBalanceDisplay from './AccountBalanceDisplay';
 
 const meta: Meta<typeof AccountBalanceDisplay> = {
   title: 'Component/AccountBalanceDisplay',
   component: AccountBalanceDisplay,
   tags: ['autodocs'],
-  // args: { onClick: fn() },
+  args: { onClick: fn() },
 };
 
 export default meta;
