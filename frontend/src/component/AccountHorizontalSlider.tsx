4b0175d000c70efaa2bde1a166a19545a5f47b43
james.ye <binbinye2@gmail.com>
2024-08-26 21:59:44 +0800

    draft CustomDots

--- frontend/src/component/AccountHorizontalSlider.tsx
+++ frontend/src/component/AccountHorizontalSlider.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AccountSlide, {
   AccountSlideEvent,
   AccountSlideProps,
 } from './AccountSlide';
 
-import Slider from 'react-slick';
+import Slider, { Settings } from 'react-slick';
 
-const settings = {
-  dots: true,
+interface CustomDotsProps {
+  currentSlide: number;
+  slideCount: number;
+}
+const CustomDots: React.FC<CustomDotsProps> = ({
+  currentSlide,
+  slideCount,
+}) => {
+  return (
+    <ul className="flex flex-row">
+      {Array.from({ length: slideCount! }).map((_, i) => (
+        <li key={i} className="h-2 w-2 bg-gray-100 rounded-full"></li>
+      ))}
+    </ul>
+  );
+};
+
+const settings: Settings = {
+  dots: false,
   infinite: true,
   speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
+  arrows: false,
 };
 
 interface AccountHorizontalSliderProps {
@@ -22,13 +40,22 @@ interface AccountHorizontalSliderProps {
 const AccountHorizontalSlider: React.FC<AccountHorizontalSliderProps> = ({
   slideData,
 }) => {
+  const slideCount = slideData.length;
+  const [currentSlide, setCurrentSlide] = useState(0);
   return (
-    <Slider {...settings}>
-      {slideData.map((data) => (
-        <AccountSlide key={data.accountId} {...data}></AccountSlide>
-      ))}
-    </Slider>
-    // </div>
+    <div>
+      <Slider {...settings} afterChange={(value) => setCurrentSlide(value)}>
+        {slideData.map((data) => (
+          <AccountSlide key={data.accountId} {...data}></AccountSlide>
+        ))}
+      </Slider>
+      <div className="w-screen flex justify-center relative bottom-20">
+        <CustomDots
+          slideCount={slideCount}
+          currentSlide={currentSlide}
+        ></CustomDots>
+      </div>
+    </div>
   );
 };
 
