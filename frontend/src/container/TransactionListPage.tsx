745a2fa87b78091b05b027cd7be0536ce845d917
James Ye <binbinye2@gmail.com>
2024-09-26 21:25:31 +0800

    add animation to TransactionListPage

--- frontend/src/container/TransactionListPage.tsx
+++ frontend/src/container/TransactionListPage.tsx
@@ -1,15 +1,32 @@
 import { useNavigate } from 'react-router-dom';
 import SubpageHeader from '../component/layout/SubpageHeader';
+import { useEffect, useState } from 'react';
 
 const TransactionListPage = () => {
   const navigate = useNavigate();
+  const [animationStyle, setAnimationStyle] = useState('translate-x-full');
 
   const handleDismiss = () => {
-    navigate(-1);
+    setAnimationStyle('translate-x-full');
+    setTimeout(() => {
+      navigate(-1);
+    }, 300);
   };
 
+  useEffect(() => {
+    const timeout = window.setTimeout(() => {
+      setAnimationStyle('');
+    }, 50);
+
+    return () => {
+      window.clearTimeout(timeout);
+    };
+  }, []);
+
   return (
-    <div className="absolute z-20 w-full h-full bg-gray-100">
+    <div
+      className={`absolute z-20 w-full h-full bg-gray-100 transition-transform duration-300 ${animationStyle}`}
+    >
       <SubpageHeader
         title="Transactions"
         onClick={handleDismiss}
