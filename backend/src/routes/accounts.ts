e674ae81e3cd56b05c2b1de290b16208c6348736
James Ye <binbinye2@gmail.com>
2024-10-04 21:31:29 +0800

    api to get transactions

--- backend/src/routes/accounts.ts
+++ backend/src/routes/accounts.ts
@@ -1,7 +1,26 @@
 import { Router } from 'express';
+import { getDBContext } from '../utils/getDBContext';
+import { getTransactionRecords } from '../db/getTransactionRecords';
 
 const router = Router();
+/**
+ * sample request: GET /api/accounts/12345/transactions?page=1&limit=20
+ */
+router.get('/accounts/:accountId/transactions', async (req, res, next) => {
+  const { accountId } = req.params;
+  const { page, limit } = req.query;
 
-router.get('/accounts/:accountId/transactions', (req, res) => {});
+  try {
+    const result = await getTransactionRecords(
+      getDBContext(req),
+      accountId,
+      page as string,
+      limit as string,
+    );
+    return res.json({ code: 0, ...result });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 export default router;
